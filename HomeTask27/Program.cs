c59b89dca4f5ee90c95e6959880db56d9d1c02a3
Ira Rakova <racova-ir@mail.ru>
2022-08-04 11:53:03 +0300

    add task27

---
+++ HomeTask27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while(N > 0)
+{
+    result = result + N % 10;
+    N = N / 10;
+}
+Console.WriteLine($"Результат = {result}");
